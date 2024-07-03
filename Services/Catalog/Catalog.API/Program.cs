0b513fb7a1b11a6e11f9f9c1f32c2eb48be253e9
Guilherme Manteuffel <guilhermemb11@gmail.com>
2024-07-03 00:05:27 -0300

    fix: Carter config (needs to be on API project to see assembly)

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container here, before build()
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // configure the HTTP request pipeline, after build()
+app.MapCarter();
 
 app.Run();
