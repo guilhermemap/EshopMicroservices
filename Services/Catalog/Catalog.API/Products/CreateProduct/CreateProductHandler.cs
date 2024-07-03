e79aaff808411b71b395c7bc2204a2e63817105c
Guilherme Manteuffel <guilhermemb11@gmail.com>
2024-07-02 22:58:33 -0300

    create method is a ICommand, not a IRequest, because it's a write operation, not readonly

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -7,12 +8,12 @@ public record CreateProductCommand(string Name,
                                    string Description,
                                    string ImageFile,
                                    decimal Price)
-                                   : IRequest<CreateProductResult>;
+                                   : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
