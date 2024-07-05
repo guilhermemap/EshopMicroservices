c80145df5b495e2d4671fcc119d4d60a1b71d181
Guilherme Manteuffel <guilhermemb11@gmail.com>
2024-07-04 23:52:02 -0300

    get Product By Id

---
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdQueryHandler
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken)
+            ?? throw new ProductNotFoundExpeption();
+
+        return new GetProductByIdResult(product);
+    }
+}
