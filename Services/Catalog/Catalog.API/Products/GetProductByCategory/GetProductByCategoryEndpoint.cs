b97f974ee5c236eb75ef658b1fc0d4efa3270892
Guilherme Manteuffel <guilhermemb11@gmail.com>
2024-07-05 00:04:24 -0300

    feat: GetProduct by Category

---
+++ Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+//public record GetProductByCategoryRequest();
+public record GetProductByCategoryResponse(Product Product);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(id));
+
+            var response = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category.");
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(Guid Id) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(Product Product);
+
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query,
+                                                         CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken)
+            ?? throw new ProductNotFoundExpeption();
+
+        return new GetProductByCategoryResult(product);
+    }
+}
